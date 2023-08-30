4997f1e1d3b6ffae14b27be9842a236986e6657d
DramixDW <verliefden.romain@gmail.com>
2023-08-30 14:05:30 +0200

    Chapitre 3: Forms: Template forms: exercice
    Formulaire d'ajout de cours

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TemplateFormsComponent } from './components/forms/template-forms/template-forms.component';
 import { FormsModule } from '@angular/forms';
+import { CourseFormComponent } from './components/forms/course-form/course-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormsComponent
+    TemplateFormsComponent,
+    CourseFormComponent
   ],
   imports: [
     BrowserModule,
